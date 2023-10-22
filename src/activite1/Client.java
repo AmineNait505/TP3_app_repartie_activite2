ed8c17afa2b685477879da7431e3a575655a7269
Nait Slimene Amine <137489572+AmineNait505@users.noreply.github.com>
2023-10-22 20:36:23 +0100

    commit

--- src/activite1/Client.java
+++ src/activite1/Client.java
@@ -1,14 +1,20 @@
 package activite1;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
-
 public class Client {
 
-	public static void main(String[] args) throws IOException {
-		Socket socket = new Socket("localhost", 1234);
-		System.out.println("je suis connecté");
-		socket.close();
-	}
+	public static void main(String[] args) throws IOException, IOException {
+        InetAddress serverAddress = InetAddress.getByName("10.26.15.106");
+        InetSocketAddress serverSocketAddress = new InetSocketAddress(serverAddress, 1234);
 
+        Socket clientSocket = new Socket();
+        clientSocket.connect(serverSocketAddress);
+
+        System.out.println("Connecté au serveur " + serverSocketAddress);	
+		clientSocket.close();
+
+	}
 	
 }
